4a46d27b9270bcdb63c6786d6000ee5d62a63f9e
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2024-06-07 13:17:06 +0530

    2

---
+++ Week2/twttr.py
@@ -0,0 +1,15 @@
+inp = input("Input: ")
+for char in inp:
+    if char.lower()=='a':
+        None
+    elif char.lower()=='e':
+        None
+    elif char.lower()=='i':
+        None
+    elif char.lower()=='o':
+        None
+    elif char.lower()=='u':
+        None
+    else:
+        print(char,end="")       
+    
